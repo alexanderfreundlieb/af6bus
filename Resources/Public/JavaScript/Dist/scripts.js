849285e8fb38da8b033b81aaa8b4fbab6dfa932d
alexanderfreundlieb <imp.paradonix@gmail.com>
2023-06-27 22:41:08 +0200

    [chore] initial commit

---
+++ Resources/Public/JavaScript/Dist/scripts.js
@@ -0,0 +1,6 @@
+/*!
+ * Sitepackage Alexander Freundlieb v1.0.0 (https://alexanderfreundlieb.ch)
+ * Copyright 2017-2023 Alexander Freundlieb
+ * Licensed under the GPL-2.0-or-later license
+ */
+console.log("WE LOVE TYPO3");

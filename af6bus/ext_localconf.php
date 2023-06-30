909ead28da95d7122912cb51a6b25db58c866caa
alexanderfreundlieb <imp.paradonix@gmail.com>
2023-06-30 23:02:20 +0200

    [feat] add pagets

---
+++ af6bus/ext_localconf.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+defined('TYPO3') or die();
+
+/* Typoscript */
+// add pagets
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig("@import 'EXT:af6bus/Configuration/TypoScript/PageTS/pagets.typoscript'");

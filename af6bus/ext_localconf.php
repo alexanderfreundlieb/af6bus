d3eae7810dcec2ad7f898056524a250710d975dc
alexanderfreundlieb <imp.paradonix@gmail.com>
2023-06-30 23:27:17 +0200

    [chore] add icon

--- af6bus/ext_localconf.php
+++ af6bus/ext_localconf.php
@@ -6,3 +6,15 @@ defined('TYPO3') or die();
 /* Typoscript */
 // add pagets
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig("@import 'EXT:af6bus/Configuration/TypoScript/PageTS/pagets.typoscript'");
+
+/* Icons */
+// register icons
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+    \TYPO3\CMS\Core\Imaging\IconRegistry::class
+);
+
+$iconRegistry->registerIcon(
+    'banner-icon', // Icon-Identifier, z.B. tx-myext-action-preview
+    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+    ['source' => 'EXT:af6bus/Resources/Public/Icons/banner.svg']
+);

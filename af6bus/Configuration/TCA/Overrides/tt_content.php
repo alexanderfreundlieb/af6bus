2dfc8e1bbc00ece40f995e46b52a74ace5ee2ebe
alexanderfreundlieb <imp.paradonix@gmail.com>
2023-07-15 22:31:54 +0200

    [feat] add new content element banner

--- af6bus/Configuration/TCA/Overrides/tt_content.php
+++ af6bus/Configuration/TCA/Overrides/tt_content.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 defined('TYPO3') or die();
 
+/* Banner */
 // Adds the content element to the "Type" dropdown
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
     'tt_content',
@@ -19,3 +20,26 @@ defined('TYPO3') or die();
     'before'
 );
 
+// Configure the default backend fields for the content element
+$GLOBALS['TCA']['tt_content']['types']['af6bus_banner'] = array(
+    'showitem' => '
+         --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.general;general,
+         assets,
+		 bodytext,
+      --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.appearance,
+         --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.frames;frames,
+      --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
+         --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.visibility;visibility,
+         --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.access;access,
+      --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.extended
+');
+
+// edit standard fields for "t8jetpack_banner" Elements
+$GLOBALS['TCA']['tt_content']['types']['af6bus_banner']['columnsOverrides'] = array(
+    // change label for Banner Text
+    'bodytext' => [
+        'config' => [
+            'enableRichtext' => true,
+        ]
+    ],
+);

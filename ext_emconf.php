637c27b6753e446651dec02a9ec88a87f7634a43
alexanderfreundlieb <imp.paradonix@gmail.com>
2023-08-05 17:34:26 +0200

    [chore] Update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,17 @@
  * Extension Manager/Repository config file for ext "af6bus".
  */
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'af6bus',
-    'description' => 'Sitepackage used for all websites developed by Alexander Freundlieb',
+    'title' => 'AF6Bus',
+    'description' => 'Main sitepackage used for all websites developed by Alexander Freundlieb',
     'category' => 'templates',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-12.4.99',
             'fluid_styled_content' => '12.4.0-12.4.99',
             'rte_ckeditor' => '12.4.0-12.4.99',
+            'ws_scss' => '^12.0',
+		    'container' => '^2.2',
+            'content_defender' => '^3.2'
         ],
         'conflicts' => [
         ],
